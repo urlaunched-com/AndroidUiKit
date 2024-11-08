ceca962b18cc5f9287379feece56f60f3c3401fa
Stanislav Ovchynnikov <stanislav.ovchinnikov.90@gmail.com>
2024-11-08 15:19:55 +0200

    code cleanup

--- common/src/main/java/com/urlaunched/android/common/viewmodel/ViewModelExtensions.kt
+++ common/src/main/java/com/urlaunched/android/common/viewmodel/ViewModelExtensions.kt
@@ -1,8 +1,6 @@
 package com.urlaunched.android.common.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.google.firebase.crashlytics.ktx.crashlytics
-import com.google.firebase.ktx.Firebase
 import com.urlaunched.android.common.response.ErrorData
 import com.urlaunched.android.common.response.Response
 
@@ -30,38 +28,3 @@ inline fun ViewModel.loadingTask(setLoading: (isLoading: Boolean) -> Unit, block
         setLoading(false)
     }
 }
-
-private fun <T : Any> getUseCaseParams(useCase: suspend () -> Response<T>): String {
-    val useCaseClass = useCase::class.java
-    return useCaseClass.declaredFields
-        .filterNot { it.name in setOf("label", "this$0") }
-        .joinToString { field ->
-            field.isAccessible = true
-            val value = field.get(useCase)
-            "${field.name.removePrefix("$")} = ${value?.toString()}"
-        }
-}
-
-private fun logFirebaseMessage(params: String, responseData: Any, isSuccess: Boolean, isUseCaseLoggable: Boolean) {
-    if (isUseCaseLoggable) {
-        val data = if (isSuccess) {
-            when {
-                responseData is Unit -> SUCCESS_MESSAGE.format(UNIT)
-                (responseData as? List<*>)?.isEmpty() == true -> SUCCESS_MESSAGE.format(EMPTY_LIST)
-                else -> SUCCESS_MESSAGE.format(responseData.toString())
-            }
-        } else {
-            ERROR_MESSAGE.format(responseData)
-        }
-
-        val fireBaseMessage = FIREBASE_MESSAGE.format(params, data)
-
-        Firebase.crashlytics.log(fireBaseMessage)
-    }
-}
-
-const val FIREBASE_MESSAGE = "performUseCase(%s) %s"
-const val SUCCESS_MESSAGE = "Success: %s"
-const val ERROR_MESSAGE = "Error: %s"
-const val UNIT = "Unit"
-const val EMPTY_LIST = "Empty list"
