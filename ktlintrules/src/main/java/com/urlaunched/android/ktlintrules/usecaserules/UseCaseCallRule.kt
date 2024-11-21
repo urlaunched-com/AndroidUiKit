203f283495060d35d3825cca01bc53b8f61bc136
Stanislav Ovchynnikov <stanislav.ovchinnikov.90@gmail.com>
2024-11-21 16:04:53 +0200

    add UseCases rules

---
+++ ktlintrules/src/main/java/com/urlaunched/android/ktlintrules/usecaserules/UseCaseCallRule.kt
@@ -0,0 +1,53 @@
+package com.urlaunched.android.ktlintrules.usecaserules
+
+import com.pinterest.ktlint.core.ast.ElementType
+import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
+import com.pinterest.ktlint.rule.engine.core.api.Rule
+import com.pinterest.ktlint.rule.engine.core.api.RuleAutocorrectApproveHandler
+import com.pinterest.ktlint.rule.engine.core.api.RuleId
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.psi.KtCallExpression
+import org.jetbrains.kotlin.psi.KtDotQualifiedExpression
+import org.jetbrains.kotlin.psi.KtNameReferenceExpression
+
+class UseCaseCallRule :
+    Rule(
+        RuleId(CUSTOM_RULE_ID),
+        about = About()
+    ),
+    RuleAutocorrectApproveHandler {
+    override fun beforeVisitChildNodes(
+        node: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision
+    ) {
+        if (node.elementType == ElementType.CALL_EXPRESSION) {
+            val callExpression = node.psi as? KtCallExpression ?: return
+            val parentExpression = callExpression.parent
+            if (parentExpression is KtDotQualifiedExpression) {
+                val receiverExpression = parentExpression.receiverExpression as? KtNameReferenceExpression
+                val selectorExpression = parentExpression.selectorExpression
+
+                if (receiverExpression != null) {
+                    val receiverName = receiverExpression.getReferencedName()
+                    if (receiverName.endsWith(USE_CASE_POSTFIX) &&
+                        selectorExpression == callExpression &&
+                        callExpression.calleeExpression?.text == INVOKE
+                    ) {
+                        emit(
+                            node.startOffset,
+                            ERROR_MESSAGE.format(receiverName),
+                            false
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CUSTOM_RULE_ID = "ktlintrules:usecasecallrule"
+        private const val ERROR_MESSAGE = "`UseCase` calls should not use `.invoke()`. Use `$%s()` instead."
+        private const val USE_CASE_POSTFIX = "UseCase"
+        private const val INVOKE = "invoke"
+    }
+}
---
+++ ktlintrules/src/main/java/com/urlaunched/android/ktlintrules/usecaserules/UseCaseConstructorParamNamingRule.kt
@@ -0,0 +1,48 @@
+package com.urlaunched.android.ktlintrules.usecaserules
+
+import com.pinterest.ktlint.rule.engine.core.api.AutocorrectDecision
+import com.pinterest.ktlint.rule.engine.core.api.ElementType
+import com.pinterest.ktlint.rule.engine.core.api.RuleAutocorrectApproveHandler
+import com.pinterest.ktlint.rule.engine.core.api.RuleId
+import org.jetbrains.kotlin.com.intellij.lang.ASTNode
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
+
+class UseCaseConstructorParamNamingRule :
+    com.pinterest.ktlint.rule.engine.core.api.Rule(
+        RuleId(CUSTOM_RULE_ID),
+        about = About()
+    ),
+    RuleAutocorrectApproveHandler {
+    override fun beforeVisitChildNodes(
+        node: ASTNode,
+        emit: (offset: Int, errorMessage: String, canBeAutoCorrected: Boolean) -> AutocorrectDecision
+    ) {
+        if (node.elementType == ElementType.CLASS) {
+            val psiClass = node.psi as? KtClass ?: return
+            val primaryConstructor = psiClass.primaryConstructor ?: return
+
+            primaryConstructor.valueParameters.forEach { param ->
+                val paramName = param.name ?: return@forEach
+                val paramType = param.typeReference?.text ?: return@forEach
+
+                if (paramType.endsWith(USE_CASE_POSTFIX)) {
+                    val expectedParamName = paramType.replaceFirstChar { it.lowercase() }
+                    if (paramName != expectedParamName) {
+                        emit(
+                            param.startOffset,
+                            ERROR_MESSAGE.format(paramName, expectedParamName, paramType),
+                            false
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CUSTOM_RULE_ID = "ktlintrules:usecaseconstructorrule"
+        private const val USE_CASE_POSTFIX = "UseCase"
+        private const val ERROR_MESSAGE = "Constructor parameter '%s' should be named '%s' for the use case type '%s'"
+    }
+}
